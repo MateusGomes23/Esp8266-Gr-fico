2353ef800813a91a7f0b31fb72890b03a66fa390
MateusGomes23 <55302655+MateusGomes23@users.noreply.github.com>
2019-09-20 13:33:24 -0300

    Create conexaoBD.php

---
+++ conexaoBD.php
@@ -0,0 +1,9 @@
+<?php
+$mysqli = new mysqli('localhost', 'USUARIO_BD', 'SENHA_BD', 'NOME_BD');
+
+if ($mysqli->connect_error) {
+    die('Erro de conexão: (' . $mysqli->connect_error);
+}
+
+date_default_timezone_set('America/Sao_Paulo');
+?>
